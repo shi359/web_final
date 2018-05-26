89e16cd9abbb4710c0e202642ba71b0e5bdc8319
tinghan <tinghan33704@gmail.com>
2018-05-26 17:46:57 +0800

    add some pages and example template

--- index.js
+++ index.js
@@ -25,10 +25,24 @@ $('.carousel').carousel({
 });
 
 $(document).ready(function() {
-    $('.dropdown').on('show.bs.dropdown', function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideDown(200);
-    });
-    $('.dropdown').on('hide.bs.dropdown', function() {
-        $(this).find('.dropdown-menu').first().stop(true, true).slideUp(200);
+    $('.pop_title').click(function() {
+        var drop_number = $(this).parent().find('.drop > div').length;
+        if($(this).parent().find('.drop').css('display') == 'none')
+        {
+            $(this).parent().find('.drop').css('display', 'block');
+            $(this).css({'background-color': 'rgb(121,38,108)', 'color': '#FFFFFF'});
+            $(this).parent().find('.drop').animate({'height': drop_number*50+"px"}, 300);
+            $(this).parent().find('.drop').find('div').animate({'margin': '20px auto', 'box-shadow': '20px auto'}, 300);
+        }
+        else if($(this).parent().find('.drop').css('display') == 'block')
+        {
+            $(this).delay(300).queue(function(next){ 
+                $(this).parent().find('.drop').css('display', 'none'); 
+                next(); 
+            });
+            $(this).css({'background-color': '#FFFFFF', 'color': '#000000'});
+            $(this).parent().find('.drop').animate({'height': '0px'}, 300);
+            $(this).parent().find('.drop').find('div').animate({'margin': '20px auto'}, 300);
+        }
     });
 });
